6decc5fa5aaa04188f11e1d71e4699de6eae8839
Liubov Peshkova <maximliuba2@gmail.com>
2024-05-21 15:45:03 +0200

    add method add usercart

--- src/main/java/de/ait/pool/repository/CartProductRepository.java
+++ src/main/java/de/ait/pool/repository/CartProductRepository.java
@@ -1,11 +1,12 @@
 package de.ait.pool.repository;
 
 import de.ait.pool.models.cart.CartProduct;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import java.util.Optional;
 
-public interface CartProductRepository extends CrudRepository<CartProduct, Long> {
+public interface CartProductRepository extends JpaRepository<CartProduct, Long> {
     Optional<CartProduct> findByCart_IdAndProduct_Id(Long cartId, Long productId);
 
 }
