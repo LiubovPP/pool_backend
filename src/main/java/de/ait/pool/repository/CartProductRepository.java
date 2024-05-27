09d09380fa2b4a6f26107eb99286f8e4ef03d887
Natalia Bodnaryuk <132370042+nata-bo@users.noreply.github.com>
2024-05-27 12:46:50 +0200

    add findByCartId()

--- src/main/java/de/ait/pool/repository/CartProductRepository.java
+++ src/main/java/de/ait/pool/repository/CartProductRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import java.util.Optional;
+import java.util.Set;
 
 public interface CartProductRepository extends JpaRepository<CartProduct, Long> {
     //    CartProduct findByCart_IdAndProduct_Id(Long cartId, Long productId);
     CartProduct findByCartIdAndProductId(Long cartId, Long productId);
+
+    Set<CartProduct> findByCartId(Long cartId);
 }
